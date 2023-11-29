1f5deb07b47bc7a955d4af70480a17161ce46cd2
William Lee <wlee753159@gmail.com>
2023-11-29 23:40:31 +0000

    Add installation script

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+. ./lib/logging
+
+echo -e "$INFO Checking dependencies..."
+
+type 'gpg' > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  echo -e "$INFO Installing gpg"
+  sudo apt install gpg -y --no-install-recommends
+else
+  echo -e "$INFO gpg already installed"
+fi
+
+type 'pandoc' > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  echo -e "$INFO Installing pandoc"
+  sudo apt install pandoc -y --no-install-recommends
+else
+  echo -e "$INFO pandoc already installed"
+fi
+
+if [[ ! -f ~/.journalrc ]]; then
+  echo -e "$INFO Generating ~/.journalrc"
+  echo 'JOURNAL_DIR=$HOME/journal' > ~/.journalrc
+else
+  echo -e "$INFO Found ~/.journalrc"
+fi
+
+echo -e "$INFO Installation done."
