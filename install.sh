104909645ec832f1cf9a3595f81e890086e2f33b
William Lee <wlee753159@gmail.com>
2023-12-29 14:10:26 +0000

    Add commands to fetch files from repository in install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 mkdir -p ~/.journaller
 cd ~/.journaller
 
-git init
-git pull --depth 1 https://github.com/WilliamWFLee/journaller.git
+echo 'Downloading files from Git repository...'
+git init -q
+git pull -q --depth 1 https://github.com/WilliamWFLee/journaller.git > /dev/null
 
+echo 'Running setup script...'
 bin/setup
