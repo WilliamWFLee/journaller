6b0b86910e90680e82585870804d7502a20c6534
William Lee <wlee753159@gmail.com>
2023-12-29 14:32:04 +0000

    Improve Git commands in install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,13 @@ cd ~/.journaller
 
 echo 'Downloading files from Git repository...'
 git init -q
-git pull -q --depth 1 https://github.com/WilliamWFLee/journaller.git > /dev/null
+git fetch -q --depth 1 https://github.com/WilliamWFLee/journaller.git > /dev/null
+if [[ $? -ne 0 ]]; then
+  echo 'Failed to fetch files from Git repository' > /dev/stderr
+  exit 1
+fi
+git reset --hard FETCH_HEAD
+git clean -dfx
 
 echo 'Running setup script...'
 bin/setup
