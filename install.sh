9d67cad7e76d0924b74a1dee3981ca2f9819413d
William Lee <wlee753159@gmail.com>
2023-12-29 23:49:51 +0000

    Uncomment code in install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 mkdir -p ~/.journaller
 cd ~/.journaller
 
-# echo 'Downloading files from Git repository...'
-# git init -q
-# git fetch -q --depth 1 https://github.com/WilliamWFLee/journaller.git > /dev/null
-# if [[ $? -ne 0 ]]; then
-#   echo 'Failed to fetch files from Git repository' > /dev/stderr
-#   exit 1
-# fi
-# git reset -q --hard FETCH_HEAD
-# git clean -q -dfx
+echo 'Downloading files from Git repository...'
+git init -q
+git fetch -q --depth 1 https://github.com/WilliamWFLee/journaller.git > /dev/null
+if [[ $? -ne 0 ]]; then
+  echo 'Failed to fetch files from Git repository' > /dev/stderr
+  exit 1
+fi
+git reset -q --hard FETCH_HEAD
+git clean -q -dfx
 
 echo 'Running setup script...'
 bin/setup
