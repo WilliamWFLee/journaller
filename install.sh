d6ce94e000223ad84db3bccb802aa3ca2fbac01f
William Lee <wlee753159@gmail.com>
2024-04-04 00:26:58 +0100

    Improve use of $?

--- install.sh
+++ install.sh
@@ -5,8 +5,10 @@ cd ~/.journaller
 
 echo 'Downloading files from Git repository...'
 git init -q
-git fetch -q --depth 1 https://github.com/WilliamWFLee/journaller.git > /dev/null
-if [[ $? -ne 0 ]]; then
+
+if ! ( \
+  git fetch -q --depth 1 https://github.com/WilliamWFLee/journaller.git > /dev/null \
+); then
   echo 'Failed to fetch files from Git repository' > /dev/stderr
   exit 1
 fi
